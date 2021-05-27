8bef4c2891e0aaf31bc0324b456fb0ea8d1f7d7d
Christian Câmara de Abreu <christian@MacBook-Air-de-Christian.local>
2021-05-26 22:11:08 -0300

    aula 82 2:11

---
+++ src/navegacao/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { SafeAreaView , Text} from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+
+export default props => (
+    <Text>navegacao index</Text>
+)
